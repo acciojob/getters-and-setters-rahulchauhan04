cd665d08bfa56acabe37feeaad29f7ca5b07b1f4
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-05-13 23:29:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //complete this code
 class Person {
 	constructor(name, age) {
-		this.name = name;
-		this.age = age;
+		this._name = name;
+		this._age = age;
 	}
 
 	get name(){
-		return this.name;
+		return this._name;
 	}
 
 	set age(age) {
-		return this.age;
+		return this._age;
 	}
 }
 
